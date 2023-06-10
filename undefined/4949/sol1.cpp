47a26ec76485f26bc29a3f49049bbbd314572497
taeheongo <taeheon9966@icloud.com>
2023-06-10 20:54:19 +0900

    4949 균형잡힌 세상

---
+++ undefined/4949/sol1.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s;
+
+bool check(string s)
+{
+    stack<char> stk;
+
+    for (char c : s)
+    {
+        if (c == '(' || c == '[')
+        {
+            stk.push(c);
+        }
+        else if (c == ')')
+        {
+            if (stk.size() && stk.top() == '(')
+                stk.pop();
+            else
+                return false;
+        }
+        else if (c == ']')
+        {
+            if (stk.size() && stk.top() == '[')
+                stk.pop();
+            else
+                return false;
+        }
+    }
+
+    return stk.empty();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while (true)
+    {
+        getline(cin, s);
+
+        if (s == ".")
+        {
+            break;
+        }
+
+        if (check(s))
+            cout << "yes\n";
+        else
+            cout << "no\n";
+    }
+
+    return 0;
+}
