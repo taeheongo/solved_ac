9d5b68f86e58b0ae245bc6b13f22402ba0c1ccec
taeheongo <taeheon9966@icloud.com>
2023-06-21 07:19:54 +0900

    12851 숨바꼭질2

---
+++ gold/12851/sol1.cpp
@@ -0,0 +1,76 @@
+/*
+    submission : https://www.acmicpc.net/source/62304294
+
+    처음에 풀었던 풀이
+
+    solution 2와의 차이점.
+    1. go라는 헬퍼 함수를 만들었는데 for문으로도 가능한 부분이기 때문에 recursive보다는 iterative 방식이 더 효율적
+    2. go 함수에서
+
+        if (visited[next_m] && visited[next_m] < visited[m] + 1)
+            return;
+        이 부분은 bfs를 완벽하게 이해하지 못한 부분이다.
+        bfs는 최단거리들을 다 순회하면서 가기때문에 딱 세가지 경우의 수만이 존재한다.
+        visited[next_m] == 0 인 경우
+        visited[next_m] < visited[m] + 1 인 경우
+        visited[next_m] == visited[m] + 1 인 경우
+
+        visited[next_m] > visited[m] + 1 인 경우는 나올 수가 없다.
+        이게 이 문제의 핵심이다.
+
+    time: O(N) 모든 노드를 순회하는 경우의 수
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+pair<int, int> ret = {100004, 0}; // {거리, 개수}
+int visited[100004];
+
+void go(int next_m, queue<int> &q, int m)
+{
+    if (next_m < 0 || next_m > 100000)
+        return;
+
+    if (visited[next_m] && visited[next_m] < visited[m] + 1)
+        return;
+
+    q.push(next_m);
+    visited[next_m] = visited[m] + 1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> k;
+
+    visited[n] = 1;
+    queue<int> q;
+    q.push(n);
+
+    while (q.size())
+    {
+        int m = q.front();
+        q.pop();
+
+        if (m == k)
+        {
+            ret.first = visited[m];
+            ret.second++;
+        }
+
+        go(m - 1, q, m);
+        go(m + 1, q, m);
+        go(m * 2, q, m);
+    }
+
+    cout << ret.first - 1 << "\n";
+    cout << ret.second << "\n";
+
+    return 0;
+}
---
+++ gold/12851/sol2.cpp
@@ -0,0 +1,94 @@
+/*
+    submission : https://www.acmicpc.net/source/62324885
+
+    solution 1에서의 문제점들을 개선한 코드
+    time: O(N)
+
+    이번에는 최단거리로 가는 개수를 구하기 위한 cnt 배열을 사용했는데
+    예를 들어, n = 5 k =17 이라고 하면
+
+    2 -> 17
+    2를 거쳐서 최종적으로 k인 17에 도달했고
+    이 경우의 수(cnt[k]) = 2, visited[k] = 4 라고 하자.
+
+    이 이후에 2로 3번만에 가는(visited[k] = 3)의 경우의 수는 더 나오지 않는다.
+    그게 bfs이다. 최단거리를 다 탐색하면서 가기때문.
+
+    그래서 딱 2가지 케이스만 고려해주면 된다.
+
+    if (!visited[next_m]) // 한번도 방문하지 않았을 때
+    {
+        visited[next_m] = visited[m] + 1;
+        q.push(next_m);
+        cnt[next_m] += cnt[m];
+    }
+    else if (visited[next_m] == visited[m] + 1) // 같은 depth일 때
+    {
+        cnt[next_m] += cnt[m];
+    }else{
+        // 이 이후에는 더 큰 depth 이기 때문에 패스
+        // (visited[next_m] < visited[m] + 1 인 케이스)
+    }
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+// 문제에서 0 <= n, k <= 100000 라고 했으므로 조심.
+int visited[100004];
+int cnt[100004];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> k;
+
+    // n == k가 같을 때는 이 문제에서는 이 부분이 없어도 통과되지만 항상 체크하는 습관을 들이는게 좋다.
+    if (n == k)
+    {
+        cout << 0 << "\n";
+        cout << 1 << "\n";
+
+        return 0;
+    }
+
+    visited[n] = 1;
+    cnt[n] = 1;
+
+    queue<int> q;
+    q.push(n);
+
+    while (q.size())
+    {
+        int m = q.front();
+        q.pop();
+
+        for (int next_m : {m + 1, m - 1, m * 2})
+        {
+            // 문제에서 0 <= n, k <= 100000 라고 했으므로 조심.
+            if (next_m < 0 || next_m > 100000)
+                continue;
+
+            if (!visited[next_m])
+            {
+                visited[next_m] = visited[m] + 1;
+                q.push(next_m);
+                cnt[next_m] += cnt[m];
+            }
+            else if (visited[next_m] == visited[m] + 1)
+            {
+                cnt[next_m] += cnt[m];
+            }
+        }
+    }
+
+    cout << visited[k] - 1 << "\n";
+    cout << cnt[k] << "\n";
+
+    return 0;
+}
