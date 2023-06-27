cca881a57f948389ef620145be97fc7bcfa742e9
taeheongo <taeheon9966@icloud.com>
2023-06-27 14:11:53 +0900

    15684 사다리 조작

---
+++ gold/15684/sol1.cpp
@@ -0,0 +1,85 @@
+/*
+    submission : https://www.acmicpc.net/source/62596084
+
+    brute force (시간초과)
+        추가된 사다리 개수 > 3 이거나 사다리의 최소개수 이상이면 go함수를 종료해야한다.
+
+    total = sum (i= 0 to 300) (300 comb i);
+
+    time <= total * n * h
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX_CNT = 32;
+
+int n, m, h, a, b, ret = MAX_CNT;
+bool visited[34][14];
+
+bool check()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        int start = i;
+        for (int j = 1; j <= h; j++)
+        {
+            if (visited[j][start])
+                start++;
+            else if (visited[j][start - 1])
+                start--;
+        }
+
+        if (start != i)
+            return false;
+    }
+
+    return true;
+}
+
+void go(int y, int cnt)
+{
+    if (check())
+    {
+        ret = min(ret, cnt);
+        return;
+    }
+
+    for (int i = y; i <= h; i++)
+    {
+        for (int j = 1; j <= n - 1; j++)
+        {
+            if (visited[i][j - 1] || visited[i][j] || visited[i][j + 1])
+                continue;
+
+            visited[i][j] = 1;
+            go(i, cnt + 1);
+            visited[i][j] = 0;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m >> h;
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a >> b;
+        visited[a][b] = 1;
+    }
+
+    go(1, 0);
+
+    if (ret == MAX_CNT)
+        cout << -1 << "\n";
+    else
+        cout << ret << "\n";
+
+    return 0;
+}
